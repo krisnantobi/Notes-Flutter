fa937ff7fbf1815ba0348c184b81bc9d8fd74c10
kris <kris@gmail.com>
2021-03-28 22:09:53 +0700

    debug false

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await sharedPrefs.init();
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     theme: ThemeData(
       primarySwatch: Colors.indigo,
     ),
